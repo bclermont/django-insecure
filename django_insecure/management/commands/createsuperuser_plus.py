ce529583fc02db20a5ea2b34f1ee8f3d4538a14e
Bruno Clermont <patate@fastmail.cn>
2013-08-14 20:08:41 +0800

    simplify command line

--- django_insecure/management/commands/createsuperuser_plus.py
+++ django_insecure/management/commands/createsuperuser_plus.py
@@ -1,23 +1,26 @@
 # -*- coding: utf-8 -*-
 
 from optparse import make_option
-from django.contrib.auth.management.commands import createsuperuser
-from django.core.management.base import CommandError
+from django.core.management.base import BaseCommand, CommandError
+from django.core.management import call_command
 from django.contrib.auth.models import User
 
-class Command(createsuperuser.Command):
-    option_list = createsuperuser.Command.option_list + (
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--username', dest='username', default=None,
+                    help='Specifies the username for the superuser.'),
+        make_option('--email', dest='email', default=None,
+                    help='Specifies the email address for the superuser.'),
         make_option('--password', dest='password', default=None,
-                    help='Specify superuser password.'),
+                    help='Specify superuser password.')
     )
 
     def handle(self, *args, **options):
-        password = options.get('password', None)
-        # force interactive, force --user
-        options['interactive'] = True
-        if not password:
-            raise CommandError("Missing --password")
-        createsuperuser.Command.handle(self, *args, **options)
+        for key in ('password', 'username', 'email'):
+            if key not in options:
+                raise CommandError('Missing --%s' % key)
+        options['interactive'] = 0
+        call_command('createsuperuser', **options)
         user = User.objects.get(username=options.get('username'))
-        user.set_password(password)
+        user.set_password(options['password'])
         user.save()
