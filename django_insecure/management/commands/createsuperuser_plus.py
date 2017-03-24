83610857d7d4bc0d1bcf2fe3771736ca7b7eca78
Bruno Clermont <bruno@robotinfra.com>
2017-03-24 11:35:58 +0800

    add --ignore_existing

--- django_insecure/management/commands/createsuperuser_plus.py
+++ django_insecure/management/commands/createsuperuser_plus.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.core.management.base import BaseCommand, CommandError
+from django.db.utils import IntegrityError
 
 try:
     from django.contrib.auth import get_user_model
@@ -23,11 +24,20 @@ class Command(BaseCommand):
                 '--password',
                 help='Specify superuser password.'
             )
+            parser.add_argument(
+                '--ignore_existing',
+                type=bool,
+                help='Do not fail if user already exists'
+            )
 
     def handle(self, *args, **options):
         for key in ('password', 'username', 'email'):
             if key not in options:
                 raise CommandError('Missing --%s' % key)
         options['interactive'] = 0
-        User.objects.create_superuser(options['username'], options['email'],
-                                      options['password'])
+        try:
+            User.objects.create_superuser(options['username'], options['email'],
+                                          options['password'])
+        except IntegrityError, err:
+            if not options['ignore_existing']: 
+                raise err
